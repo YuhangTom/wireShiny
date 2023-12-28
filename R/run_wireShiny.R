7986dd862396d9c2c39e09a789eed5fa9dd4072f
Yuhang (Tom) Lin <yhlin@iastate.edu>
2023-12-28 17:54:12 -0600

    App setup with correct folder structure, sample ui and server
    https://deanattali.com/2015/04/21/r-package-shiny-app/

---
+++ R/run_wireShiny.R
@@ -0,0 +1,18 @@
+#' Run the Shiny app based on functionalities of `wire`
+#'
+#' @importFrom shiny runApp
+#' @export
+#' @examples
+#' if (interactive()) {
+#'   run_wireShiny()
+#' }
+#'
+run_wireShiny <- function() {
+  appDir <- system.file("wireShinyApp", package = "wireShiny")
+
+  if (appDir == "") {
+    stop("Could not find the Shiny app directory. Try re-installing `wireShiny`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
