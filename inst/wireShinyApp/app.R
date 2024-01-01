1f0043fc8f6308318ea98d2fe8d51cb5287b4c70
Yuhang (Tom) Lin <yhlin@iastate.edu>
2024-01-01 09:03:37 -0600

    Clear button behaviors

--- inst/wireShinyApp/app.R
+++ inst/wireShinyApp/app.R
@@ -1,23 +1,24 @@
 library(shiny)
+library(shinyjs)
 library(x3ptools)
 library(assertthat)
 
 
 ui <- fluidPage(
-  shinyjs::useShinyjs(),  # Set up shinyjs
+  useShinyjs(), # Set up shinyjs
 
   titlePanel("R Shiny App for wire"),
-
   sidebarLayout(
     sidebarPanel(
       fileInput("fileInput1", "Choose file (Max size: 5MB, Acceptable formats: .x3p, .rda)", accept = c(".x3p", ".rda")),
-      shinyjs::hidden(  # Hide the second file input initially
-        div(id = "secondFileInput",  # Add an id to the div so it can be referenced
-            fileInput("fileInput2", "Choose file (Max size: 5MB, Acceptable format: .x3p)", accept = c(".x3p"))
+      hidden( # Hide the second file input initially
+        div(
+          id = "secondFileInput", # Add an id to the div so it can be referenced
+          fileInput("fileInput2", "Choose file (Max size: 5MB, Acceptable format: .x3p)", accept = c(".x3p"))
         )
-      )
+      ),
+      actionButton("clear", "Clear files") # Add a button to clear the file inputs
     ),
-
     mainPanel(
       verbatimTextOutput("strOutput")
     )
@@ -28,29 +29,32 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-  observeEvent(c(input$fileInput1, input$fileInput2), {
+  rv <- reactiveValues(x3p1 = NULL, x3p2 = NULL)
+
+  observeEvent(input$fileInput1, {
     inFile1 <- input$fileInput1
-    inFile2 <- input$fileInput2
 
     if (is.null(inFile1)) {
-      stop("Please upload the first file.")
+      showNotification("Please upload the first file.", type = "error")
+      return(NULL)
     }
 
     ext1 <- tools::file_ext(inFile1$datapath)
 
     if (ext1 == "x3p") {
-      shinyjs::show("secondFileInput")  # Show the second file input if the first file is a x3p file
+      show("secondFileInput") # Show the second file input if the first file is a x3p file
     } else {
-      shinyjs::hide("secondFileInput")  # Hide the second file input otherwise
+      hide("secondFileInput") # Hide the second file input otherwise
     }
 
     if (ext1 == "x3p") {
-      x3p1 <- x3ptools::x3p_read(inFile1$datapath)
+      x3p1 <- x3p_read(inFile1$datapath)
       assert_that("x3p" %in% class(x3p1), msg = "The first uploaded file is not a valid x3p file.")
     } else if (ext1 == "rda") {
       x3pList <- get(load(inFile1$datapath))
       if (!is.list(x3pList) || length(x3pList) < 2 || !all(sapply(x3pList, function(x) "x3p" %in% class(x)))) {
-        stop("The first uploaded file is not a valid rda file containing at least 2 x3p objects.")
+        showNotification("The first uploaded file is not a valid rda file containing at least 2 x3p objects.", type = "error")
+        return(NULL)
       }
       if (length(x3pList) > 2) {
         message("More than 2 x3p objects detected in the first file. Only the first 2 x3p objects will be used.")
@@ -58,27 +62,49 @@ server <- function(input, output) {
       x3p1 <- x3pList[1]
       x3p2 <- x3pList[2]
     } else {
-      stop("Incorrect input format for the first file. Please upload a x3p or rda file.")
+      showNotification("Incorrect input format for the first file. Please upload a x3p or rda file.", type = "error")
+      return(NULL)
+    }
+
+    rv$x3p1 <- x3p1
+  })
+
+  observeEvent(input$fileInput2, {
+    inFile2 <- input$fileInput2
+
+    if (is.null(inFile2)) {
+      return(NULL)
     }
 
     if (!is.null(inFile2)) {
       ext2 <- tools::file_ext(inFile2$datapath)
       if (ext2 == "x3p") {
-        x3p2 <- x3ptools::x3p_read(inFile2$datapath)
+        x3p2 <- x3p_read(inFile2$datapath)
         assert_that("x3p" %in% class(x3p2), msg = "The second uploaded file is not a valid x3p file.")
       } else {
-        stop("Incorrect input format for the second file. Please upload a x3p file.")
+        showNotification("Incorrect input format for the first file. Please upload a x3p or rda file.", type = "error")
+        return(NULL)
       }
     }
 
-    output$strOutput <- renderPrint({
+    rv$x3p2 <- x3p2
+  })
+
+  observeEvent(input$clear, {
+    shinyjs::reset("fileInput1")
+    shinyjs::reset("fileInput2")
+    rv$x3p1 <- NULL
+    rv$x3p2 <- NULL
+    shinyjs::hide("secondFileInput") # Hide the second file input
+  })
+
+  output$strOutput <- renderPrint({
+    if (!is.null(rv$x3p1) && !is.null(rv$x3p2)) {
       cat("Structure of the first x3p object:\n")
-      str(x3p1)
-      if (exists("x3p2")) {
-        cat("\nStructure of the second x3p object:\n")
-        str(x3p2)
-      }
-    })
+      str(rv$x3p1) # Use the x3p1 from the reactive values
+      cat("\nStructure of the second x3p object:\n")
+      str(rv$x3p2) # Use the x3p2 from the reactive values
+    }
   })
 }
 
